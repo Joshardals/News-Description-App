1643c42654019ef601872ca262ab9f980cbb6260
Joshardals <joshuabamidele219@gmail.com>
2023-10-26 14:39:05 +0100

    final commit

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,38 +6,18 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 // created by chatgpt
-// export function formatDateString(dateString: string) {
-//   // Create a Date object from the provided date and time string
-//   const date = new Date(dateString);
-
-//   // Format the date and time according to the system's locale and time zone
-//   const formattedDateTime = new Intl.DateTimeFormat(undefined, {
-//     year: "numeric",
-//     month: "short",
-//     day: "numeric",
-//     hour: "numeric",
-//     minute: "2-digit",
-//     second: "2-digit",
-//   }).format(date);
-
-//   return formattedDateTime;
-// }
-
-// created by chatgpt
 export function formatDateString(dateString: string) {
   const options: Intl.DateTimeFormatOptions = {
     year: "numeric",
     month: "short",
     day: "numeric",
+    hour: "numeric",
+    minute: "2-digit",
   };
 
   const date = new Date(dateString);
-  const formattedDate = date.toLocaleDateString(undefined, options);
+  const formatter = new Intl.DateTimeFormat(undefined, options);
+  const formattedDate = formatter.format(date);
 
-  const time = date.toLocaleTimeString([], {
-    hour: "numeric",
-    minute: "2-digit",
-  });
-
-  return `${time} - ${formattedDate}`;
+  return formattedDate;
 }
